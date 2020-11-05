08e06251d319e15de46f1305d3f55c8f5948e84c
sofiul_iam <50357469+sofiull@users.noreply.github.com>
2020-11-05 23:22:50 +0700

    Membaca data json dari Database

--- Frontend - User/src/Containers/PostCollection.js
+++ Frontend - User/src/Containers/PostCollection.js
@@ -8,7 +8,7 @@ class PostCollection extends Component{
     }
 
     ambilDataDariServerAPI = () =>{
-        fetch('http://localhost:3001/gadgetList')
+        fetch('http://127.0.0.1:5000/buku')
             .then(response => response.json())
             .then(jsonHasilAmbilDariAPI => {
                 this.setState({
@@ -21,103 +21,103 @@ class PostCollection extends Component{
         this.ambilDataDariServerAPI()
     }
 
-    getDataProduk = (data) => {
-        fetch(`http://localhost:3001/gadgetList/${data}`, { method: "GET" })
-            .then(response => response.json())
-            .then(res => {
-                var Produk = { ...this.state.insertKeranjang };
-                Produk["id"] = res["id"];
-                Produk["Judul"] = res["Judul"];
-                Produk["Kategori"] = res["Kategori"];
-                Produk["Penerbit"] = res["Penerbit"];
-                Produk["Penulis"] = res["Penulis"];
-                Produk["Link"] = res["Link"];
-                Produk["stok"] = 1;
-                this.setState({
-                    insertKeranjang: Produk
-                });
-            })
-            .then(() => {
-                this.handleCekKeranjang(data);
-            })
-            .then(() => {
-                this.handleUpdateStok(data);
-            })
-            .then(() => {
-                this.handleBeli();
-            });
-    };
-
-    handleCekKeranjang = data => {
-        fetch(`http://localhost:3002/keranjang/${data}`, { method: "GET" }).then(
-            response => {
-                if (response.ok) {
-                    response.json().then(res => {
-                        this.handleUpdateKeranjang(data, res);
-                        this.ambilDataDariServerAPI();
-                    });
-                } else {
-                    this.handleBeli();
-                }
-            }
-        );
-    };
-
-    handleUpdateKeranjang = (data, res) => {
-        fetch(`http://localhost:3002/keranjang/${data}`, {
-            method: "PUT",
-            headers: {
-                Accept: "application/json",
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                Judul: res["Judul"],
-                Kategori: res["Kategori"],
-                Penerbit: res["Penerbit"],
-                Penulis: res["Penulis"],
-                Link: res["Link"]
-            })
-        });
-    };
-
-    handleBeli = () => {
-        fetch('http://localhost:3002/keranjang', {
-            method: 'post',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(this.state.insertKeranjang)
-        })
-            .then((Response) => {
-                this.ambilDataDariServerAPI();
-            });
-    }
-
-    handleUpdateStok = data => {
-        fetch(`http://localhost:3001/gadgetList/${data}`, { method: "GET" }).then(
-            response => {
-                response.json().then(res => {
-                    fetch(`http://localhost:3001/gadgetList/${data}`, {
-                        method: "PUT",
-                        headers: {
-                            Accept: "application/json",
-                            "Content-Type": "application/json"
-                        },
-                        body: JSON.stringify({
-                            Gambar: res["Gambar"],
-                            Judul: res["Judul"],
-                            Kategori: res["Kategori"],
-                            Penerbit: res["Penerbit"],
-                            Penulis: res["Penulis"],
-                            Link: res["Link"]
-                        })
-                    });
-                    this.ambilDataDariServerAPI();
-                });
-            }
-        );
-    };
+//    getDataProduk = (data) => {
+//        fetch(`http://127.0.0.1:5000/buku/${data}`, { method: "GET" })
+//            .then(response => response.json())
+//            .then(res => {
+//                var Produk = { ...this.state.insertKeranjang };
+//                Produk["id"] = res["id_buku"];
+//                Produk["Judul"] = res["Judul"];
+//                Produk["Kategori"] = res["Kategori"];
+//                Produk["Penerbit"] = res["Penerbit"];
+//                Produk["Penulis"] = res["Penulis"];
+//                Produk["Link"] = res["Link"];
+//                Produk["stok"] = 1;
+//                this.setState({
+//                    insertKeranjang: Produk
+//                });
+//            })
+//            .then(() => {
+//                this.handleCekKeranjang(data);
+//            })
+//            .then(() => {
+//                this.handleUpdateStok(data);
+//            })
+//            .then(() => {
+//                this.handleBeli();
+//            });
+//    };
+//
+//    handleCekKeranjang = data => {
+//        fetch(`http://localhost:3002/keranjang/${data}`, { method: "GET" }).then(
+//            response => {
+//                if (response.ok) {
+//                    response.json().then(res => {
+//                        this.handleUpdateKeranjang(data, res);
+//                        this.ambilDataDariServerAPI();
+//                    });
+//                } else {
+//                    this.handleBeli();
+//                }
+//            }
+//        );
+//    };
+//
+//    handleUpdateKeranjang = (data, res) => {
+//        fetch(`http://localhost:3002/keranjang/${data}`, {
+//            method: "PUT",
+//            headers: {
+//                Accept: "application/json",
+//                "Content-Type": "application/json"
+//            },
+//            body: JSON.stringify({
+//                Judul: res["Judul"],
+//                Kategori: res["Kategori"],
+//                Penerbit: res["Penerbit"],
+//                Penulis: res["Penulis"],
+//                Link: res["Link"]
+//            })
+//        });
+//    };
+//
+//    handleBeli = () => {
+//        fetch('http://localhost:3002/keranjang', {
+//            method: 'post',
+//            headers: {
+//                'Accept': 'application/json',
+//                'Content-Type': 'application/json'
+//            },
+//            body: JSON.stringify(this.state.insertKeranjang)
+//        })
+//            .then((Response) => {
+//                this.ambilDataDariServerAPI();
+//            });
+//    }
+//
+//    handleUpdateStok = data => {
+//        fetch(`http://127.0.0.1:5000/buku/${data}`, { method: "GET" }).then(
+//            response => {
+//                response.json().then(res => {
+//                    fetch(`http://127.0.0.1:5000/buku/${data}`, {
+//                        method: "PUT",
+//                        headers: {
+//                            Accept: "application/json",
+//                            "Content-Type": "application/json"
+//                        },
+//                        body: JSON.stringify({
+//                            Gambar: res["Gambar"],
+//                            Judul: res["Judul"],
+//                            Kategori: res["Kategori"],
+//                            Penerbit: res["Penerbit"],
+//                            Penulis: res["Penulis"],
+//                            Link: res["Link"]
+//                        })
+//                    });
+//                    this.ambilDataDariServerAPI();
+//                });
+//            }
+//        );
+//    };
 
     render(){
         return(
@@ -128,15 +128,15 @@ class PostCollection extends Component{
                     <div className="card-segment row">
                     {
                         this.state.ListGadget.map(buku => {
-                            return <Book key={buku.id}
-                                        id={buku.id}
-                                        Gambar = {buku.Gambar}
-                                        Judul={buku.Judul}
-                                        Kategori={buku.Kategori}
-                                        Penerbit={buku.Penerbit}
-                                        Penulis={buku.Penulis}
-                                        Link={buku.Link}
-                                        LinkReview={"review?no="+buku.id}/>
+                            return <Book key={buku.id_buku}
+                                        id={buku.id_buku}
+                                        Gambar = {buku.gambar}
+                                        Judul={buku.nama_buku}
+                                        Kategori={buku.jenis_buku}
+                                        Penerbit={buku.nama_penerbit}
+                                        Penulis={buku.nama_penulis}
+                                        Link={buku.userfile}
+                                        LinkReview={"review?no="+buku.id_buku}/>
                         })
                     }
                     </div>
